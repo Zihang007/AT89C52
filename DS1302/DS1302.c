fd660f4933faeec14445ba519d254d2e7ca22246
Zihang007 <120353978+Zihang007@users.noreply.github.com>
2023-11-17 14:49:10 +0800

    Update DS1302.c

--- DS1302/DS1302.c
+++ DS1302/DS1302.c
@@ -5,6 +5,16 @@ sbit DS1302_SCLK=P3^6;
 sbit DS1302_IO=P3^4;
 sbit DS1302_CE=P3^5;
 
+#define DS1302_SECOND    0x80
+#define DS1302_MIN       0x82
+#define DS1302_HOUR      0x84
+#define DS1302_DATE      0x86
+#define DS1302_MONTH     0x88
+#define DS1302_DAY       0x8A
+#define DS1302_YEAR      0x8C
+#define DS1302_WP        0x8E  //写入保护
+
+unsigned char DS1302_Time[]={23,11,17,14,48,00,5};   //初始化
 void DS1302_Init(void)
 {
 	DS1302_CE=0;
@@ -31,11 +41,12 @@ void DS1302_WriteByte(unsigned char Command,Data)
 	}
 	DS1302_CE=0;
 	
-}
+}//根据写入数据；
 
 unsigned char DS1302_ReadByte(unsigned char Command)
 {
 	unsigned char i,Data=0x00;
+	Command|=0x01;
 	DS1302_CE=1;
 	
 	for(i=0;i<8;i++){
@@ -51,4 +62,38 @@ unsigned char DS1302_ReadByte(unsigned char Command)
 	}	DS1302_CE=0;
 	DS1302_IO=0;
 		return Data;
+}//读出数据；
+
+void DS1302_SetTime(void)
+{
+    DS1302_WriteByte(DS1302_WP,0x00);
+    DS1302_WriteByte(DS1302_YEAR,DS1302_Time[0] / 10 * 16 + DS1302_Time[0] % 10);
+    DS1302_WriteByte(DS1302_MONTH,DS1302_Time[1] / 10 * 16 + DS1302_Time[1] % 10);
+    DS1302_WriteByte(DS1302_DATE,DS1302_Time[2] / 10 * 16 + DS1302_Time[2] % 10);
+    DS1302_WriteByte(DS1302_HOUR,DS1302_Time[3] / 10 * 16 + DS1302_Time[3] % 10);
+    DS1302_WriteByte(DS1302_MIN,DS1302_Time[4] / 10 * 16 + DS1302_Time[4] % 10);
+    DS1302_WriteByte(DS1302_SECOND,DS1302_Time[5] / 10 * 16 + DS1302_Time[5] % 10);
+    DS1302_WriteByte(DS1302_DAY,DS1302_Time[6] / 10 * 16 + DS1302_Time[6] % 10);
+    DS1302_WriteByte(DS1302_WP,0x80);
+}
+
+
+void DS1302_ReadTime(void)
+{
+	unsigned char Temp;
+	Temp=DS1302_ReadByte(DS1302_YEAR);
+	DS1302_Time[0]=Temp/16*10+Temp%16;
+	Temp=DS1302_ReadByte(DS1302_MONTH);
+	DS1302_Time[1]=Temp/16*10+Temp%16;
+	Temp=DS1302_ReadByte(DS1302_DATE);
+	DS1302_Time[2]=Temp/16*10+Temp%16;
+	Temp=DS1302_ReadByte(DS1302_HOUR);
+	DS1302_Time[3]=Temp/16*10+Temp%16;
+	Temp=DS1302_ReadByte(DS1302_MIN);
+	DS1302_Time[4]=Temp/16*10+Temp%16;
+	Temp=DS1302_ReadByte(DS1302_SECOND);
+	DS1302_Time[5]=Temp/16*10+Temp%16;
+	Temp=DS1302_ReadByte(DS1302_DAY);
+	DS1302_Time[6]=Temp/16*10+Temp%16;
+	
 }
