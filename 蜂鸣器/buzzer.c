5fc8e97ca7ba4dd88cf4b7d7fbbe3960684b5fbc
Zihang007 <120353978+Zihang007@users.noreply.github.com>
2023-11-18 16:55:59 +0800

    Update buzzer.c

--- 蜂鸣器/buzzer.c
+++ 蜂鸣器/buzzer.c
@@ -2,7 +2,7 @@
 #include"Delay.h"
 sbit Buzzer = P2^5;
 void Buzzer_Time(unsigned int ms){
-	 unsigned char i, KeyNum;
+	 unsigned char i, KeyNum=0;
             if (P3_1 == 0) {
                 Delay(20);
                 while (P3_1 == 0);
@@ -15,6 +15,5 @@ void Buzzer_Time(unsigned int ms){
                 Buzzer = !Buzzer;
                 Delay(1);
             }
-						KeyNum=0;
         }
 }
