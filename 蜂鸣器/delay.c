4041744de1a2aff1b38d4e3fdb928835f7b5b26c
Zihang007 <120353978+Zihang007@users.noreply.github.com>
2023-11-18 12:56:09 +0800

    Create delay.c

---
+++ 蜂鸣器/delay.c
@@ -0,0 +1,21 @@
+//1ms
+#include <REGX52.H>
+#include <intrins.h>
+void Delay(unsigned char time)	//@11.0592MHz
+{
+	unsigned char i_0;
+	for(i_0=0;i_0<time;i_0++){
+
+	unsigned char data i, j;
+
+	i = 2;
+	j = 239;
+	do
+	{
+		while (--j);
+	} while (--i);
+}
+
+	}
+
+
